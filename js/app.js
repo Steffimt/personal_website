fb18052335d1b80f2535c69c04f846781935bd56
Steffimt <steffimariatanasia@gmail.com>
2023-07-16 00:55:10 +0700

    ...

--- js/app.js
+++ js/app.js
@@ -7,3 +7,13 @@ ScrollSmoother.create({
     wrapper: '.wrapper',
     content: '.content'
 })
+var menu = document.getElementById("menu");
+menu.onclick = function(){
+    menu.classList.toggle("openmenu");
+}
+const menuIcon = document.querySelector(".menu-icon");
+const navLinks = document.querySelector(".nav-links");
+ 
+menuIcon.addEventListener('click',()=>{
+    navLinks.classList.toggle('mobile-menu')
+})
