678a0c83bb5ae98932df4b235dea81740953e6ed
Steffimt <steffimariatanasia@gmail.com>
2023-07-16 17:14:42 +0700

    lots of update

---
+++ js/scroll.js
@@ -0,0 +1,26 @@
+
+    // const movingElement = document.querySelector('.circle');
+    
+    // function updateElementPosition() {
+    //   // Calculate the new position based on the scrollY (vertical scroll position)
+    //   const scrollPosition = window.scrollY;
+    //   const newPosition = scrollPosition * 0.5; // Adjust the factor to control the speed of movement
+      
+    //   // Apply the transform to move the element
+    //   movingElement.style.transform = `translateY(${newPosition}px)`;
+    // }
+    
+    // Listen for the scroll event and call the updateElementPosition function
+
+
+    // window.addEventListener('scroll', updateElementPosition);
+
+    // const movingElement = document.querySelector('.circle');
+    // function updateElementPosition() {
+    //     const scrolled = window.scrollY;
+    //     const val = scrolled * 0.5;
+    //     movingElement.style.transform = `translateY(${0.5 * val}em)`;
+    // }
+
+
+    
