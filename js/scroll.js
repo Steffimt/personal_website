8c54ecdddb1b8aec3b80ca0d887bba7a7a562397
Steffimt <steffimariatanasia@gmail.com>
2023-07-17 16:17:48 +0700

    project-part1

--- js/scroll.js
+++ js/scroll.js
@@ -21,6 +21,22 @@
     //     const val = scrolled * 0.5;
     //     movingElement.style.transform = `translateY(${0.5 * val}em)`;
     // }
+    window.addEventListener('scroll', reveal);
+    function reveal(){
+        var reveals = document.querySelectorAll('.reveal');
 
+        for(var i = 0; i < reveals.length; i++){
+            var windowheight = window.innerHeight;
+            var revealtop = reveals[i].getBoundingClientRect().top;
+            var revealpoint = 50;
+
+            if(revealtop < windowheight - revealpoint){
+                reveals[i].classList.add('active');
+            }
+            else{
+                reveals[i].classList.remove('active');
+            }
+        }
+    }
 
     
