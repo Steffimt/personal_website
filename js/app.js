d825ac283d8ddb702e3258319f2d3d37183c0f5c
Steffimt <steffimariatanasia@gmail.com>
2023-07-19 20:40:13 +0700

    project pt2

--- js/app.js
+++ js/app.js
@@ -18,3 +18,27 @@ ScrollSmoother.create({
 //     navLinks.classList.toggle('mobile-menu')
 // })
 
+const handleOnMouseMove = e => {
+    const{ currentTarget: target } = e;
+
+    const rect = target.getBoundingClientRect(),
+        x = e.clientX - rect.left,
+        y = e.clientY - rect.top;
+    
+    target.style.setProperty("--mouse-x", `${x}px`);
+    target.style.setProperty("--mouse-y", `${y}px`);
+}
+
+for (const card of document.querySelectorAll(".text-card1, .text-card2, .text-card3")){
+    card.onmousemove = e => handleOnMouseMove(e);
+}
+
+document.getElementById("cards").onmousemove = e => {
+    for (const card of document.getElementsByClassName("text-card1, .text-card2, .text-card3")){
+        const rect = card.getBoundingClientRect(),
+            x = e.clientX - rect.left,
+            y = e.clientY - rect.top;
+        target.style.setProperty("--mouse-x", `${x}px`);
+        target.style.setProperty("--mouse-y", `${y}px`);
+    }
+}
