4c843a1168b150533eeb9c377220a935ae8dd4ad
Steffimt <steffimariatanasia@gmail.com>
2023-07-27 19:41:02 +0700

    project-page

--- js/projects.js
+++ js/projects.js
@@ -1,23 +1,3 @@
-window.addEventListener('scroll', e => {
-    document.body.style.cssText += `--scrollTop: ${this.scrollY}px`
-
-})
-gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
-ScrollSmoother.create({
-    wrapper: '.wrapper',
-    content: '.content'
-})
-// var menu = document.getElementById("menu");
-// menu.onclick = function(){
-//     menu.classList.toggle("openmenu");
-// }
-// const menuIcon = document.querySelector(".menu-icon");
-// const navLinks = document.querySelector(".nav-links");
- 
-// menuIcon.addEventListener('click',()=>{
-//     navLinks.classList.toggle('mobile-menu')
-// })
-
 const handleOnMouseMove = e => {
     const{ currentTarget: target } = e;
 
